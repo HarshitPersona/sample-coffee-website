527d02c42f73d8ffc4fcae3d066f24f64173cfc3
Harshit Kumar <harshitkumar@Harshits-MacBook-Pro.local>
2023-10-11 11:58:49 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/sample-cryptoslate.html");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is up on " + PORT);
+});
